416ba03cd5cdcd5475ec8dff7f5f68beb5ed151a
Cesar Luis RR <romcel87@gmail.com>
2026-04-21 11:13:29 -0500

    feat: add Botbless link to header

--- resources/views/partials/desktop-cuenta-registro-carrito.blade.php
+++ resources/views/partials/desktop-cuenta-registro-carrito.blade.php
@@ -2,7 +2,10 @@
       {{-- Íconos --}}
       <div class="flex items-center space-x-3">
         {{--<a href="#" class="hover:underline">Registrate</a>--}}
-        <a href="https://blessrom.com/mi-cuenta/" class="hover:underline">Mi cuenta</a>
+        <div class="flex flex-col items-end">
+          <a href="https://blessrom.com/mi-cuenta/" class="hover:underline font-bold">Mi cuenta</a>
+          <a href="https://wa.me/51926940715" target="_blank" class="text-[10px] text-gray-300 hover:text-white transition-colors">Consulta a Botbless: +51926940715</a>
+        </div>
         <a href="https://blessrom.com/carrito/" class="relative hover:underline widget_shopping_cart_content bg-white text-white p-2 rounded-full">
           🛒
           <span id="cart-count" class="absolute -top-2 -right-2 bg-[#FFB816] text-white text-xs font-bold rounded-full px-1.5">
--- resources/views/partials/mobile-cuenta-registro-carrito.blade.php
+++ resources/views/partials/mobile-cuenta-registro-carrito.blade.php
@@ -1,5 +1,8 @@
 <div class="flex items-center gap-3">
-      <a href="https://blessrom.com/mi-cuenta/" class="hover:underline text-sm">Mi cuenta</a>
+      <div class="flex flex-col items-end">
+        <a href="https://blessrom.com/mi-cuenta/" class="hover:underline text-sm font-bold">Mi cuenta</a>
+        <a href="https://wa.me/51926940715" target="_blank" class="text-[9px] text-gray-300 hover:text-white transition-colors">Botbless: +51926940715</a>
+      </div>
       <a href="https://blessrom.com/carrito/" class="relative">
         🛒
         <span id="cart-count" class="absolute -top-2 -right-2 bg-[#FFB816] text-white text-xs font-bold rounded-full px-1.5">
